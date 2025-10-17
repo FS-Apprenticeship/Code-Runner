65954be7fdcefb6ea89b82311af41f6d010339cb
itsKia2 <omerkarimi29@gmail.com>
2025-10-16 20:07:43 -0400

    selecting language/topic page now created

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LandingPage from '@/views/LandingPage.vue'
+import SigninPage from './views/SigninPage.vue'
+import SignupPage from './views/SignupPage.vue'
+import SelectionPage from './views/SelectionPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: LandingPage },
+    { path: '/signin', component: SigninPage },
+    { path: '/signup', component: SignupPage },
+    { path: '/selection', component: SelectionPage},
   ],
 })
 
