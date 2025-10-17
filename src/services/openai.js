a8eab112c281e1ab51a82924199ddf8f158d17c1
itsKia2 <omerkarimi29@gmail.com>
2025-10-17 12:56:18 -0400

    added creating prompt function

--- src/services/openai.js
+++ src/services/openai.js
@@ -1,5 +1,20 @@
 import { supa } from "../database/auth";
 
+export function createPrompt(language, topic) {
+    const prompt = [
+        {
+            role: "user",
+            content: "say pineapple 3 times",
+        },
+        {
+            role: "developer",
+            content: "say it in a pirate accent",
+        }
+    ];
+    return prompt
+}
+
+// call this with const resp = await callCodeEvaluation(JSON.stringify({ prompt }))
 export async function callCodeEvaluation(prompt) {
     const { data, error } = await supa.functions.invoke("code-evaluation", {
         body: prompt,
